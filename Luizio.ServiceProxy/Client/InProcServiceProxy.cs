348db12b02763c736d546f3363dbbc846becf474
Ludwig Andersson <ludwig.andersson@LIVE.COM>
2024-10-29 08:37:17 +0100

    Improve logging when not being able to resolve method to invoke

--- Luizio.ServiceProxy/Client/InProcServiceProxy.cs
+++ Luizio.ServiceProxy/Client/InProcServiceProxy.cs
@@ -6,38 +6,49 @@ using Luizio.ServiceProxy.Models;
 using Luizio.ServiceProxy.Server;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace Luizio.ServiceProxy.Client;
 
 public class InProcServiceProxy : IServiceProxy
 {
-	private readonly IServiceProvider sp;
-	private readonly CurrentUser currentUser;
+    private readonly IServiceProvider sp;
+    private readonly CurrentUser currentUser;
 
-	public InProcServiceProxy(IServiceProvider sp, CurrentUser currentUser)
-	{
-		this.sp = sp;
-		this.currentUser = currentUser;
-	}
+    public InProcServiceProxy(IServiceProvider sp, CurrentUser currentUser)
+    {
+        this.sp = sp;
+        this.currentUser = currentUser;
+    }
 
-	public async Task<Response<TRes>> Invoke<T, TRes>(string appName, string serviceName, string methodName, T request)
-		where T : class, new()
-		where TRes : class
-	{
-		using var scope = sp.CreateScope();
-		var logger = scope.ServiceProvider.GetRequiredService<ILogger<InProcServiceProxy>>();
-		logger.LogInformation("Calling \"{AppName}.{ServiceName}.{MethodName}\".", appName, serviceName, methodName);
-		
-		var newCurrentUser = scope.ServiceProvider.GetRequiredService<CurrentUser>();
-		newCurrentUser.Metadata = currentUser.Metadata;
-		newCurrentUser.Token = currentUser.Token;
+    public async Task<Response<TRes>> Invoke<T, TRes>(string appName, string serviceName, string methodName, T request)
+        where T : class, new()
+        where TRes : class
+    {
+        using var scope = sp.CreateScope();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<InProcServiceProxy>>();
+        logger.LogInformation("Calling \"{AppName}.{ServiceName}.{MethodName}\".", appName, serviceName, methodName);
 
-		var serviceImpl = ServiceStore.GetService(serviceName, scope.ServiceProvider);
-		var methodToInvoke = ServiceStore.GetMethod(methodName, typeof(T), serviceImpl);
-		
-		Activity? previousActivity = Activity.Current;
-		Activity? proxyActivity = null;
-		try
+        var newCurrentUser = scope.ServiceProvider.GetRequiredService<CurrentUser>();
+        newCurrentUser.Metadata = currentUser.Metadata;
+        newCurrentUser.Token = currentUser.Token;
+
+        IService serviceImpl;
+        MethodInfo methodToInvoke;
+        try
+        {
+            serviceImpl = ServiceStore.GetService(serviceName, scope.ServiceProvider);
+            methodToInvoke = ServiceStore.GetMethod(methodName, typeof(T), serviceImpl);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Could not resolve service and method to invoke. Service name {ServiceName} with method name {MethodName}", serviceName, methodName);
+            throw;
+        }
+
+        Activity? previousActivity = Activity.Current;
+        Activity? proxyActivity = null;
+        try
         {
             if (previousActivity is not null)
             {
@@ -56,17 +67,17 @@ public class InProcServiceProxy : IServiceProxy
             return response;
         }
         catch (System.Exception e)
-		{
-			logger.LogError("Method call to {AppName}.{ServiceName}.{MethodName} failed with error {ErrorCode} - {ErrorMessage}.", appName, serviceName, methodName, ErrorCode.Exception, e.ToString());
-			proxyActivity?.SetStatus(ActivityStatusCode.Ok);
-			return new Error(ErrorCode.Exception, e.ToString());
-		}
-		finally
-		{
-			proxyActivity?.Stop();
-			Activity.Current = previousActivity;
-		}
-	}
+        {
+            logger.LogError("Method call to {AppName}.{ServiceName}.{MethodName} failed with error {ErrorCode} - {ErrorMessage}.", appName, serviceName, methodName, ErrorCode.Exception, e.ToString());
+            proxyActivity?.SetStatus(ActivityStatusCode.Ok);
+            return new Error(ErrorCode.Exception, e.ToString());
+        }
+        finally
+        {
+            proxyActivity?.Stop();
+            Activity.Current = previousActivity;
+        }
+    }
 
     private void LogResponse<TRes>(string appName, string serviceName, string methodName, ILogger<InProcServiceProxy> logger, Activity? proxyActivity, Response<TRes> response) where TRes : class
     {
